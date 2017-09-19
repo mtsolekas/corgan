0f8e4bdc90aa047df4ba600391f925422699c7ac
Marios Tsolekas <marios.tsolekas@gmail.com>
2017-09-19 19:09:40 +0300

    Alignement change

--- src/corgan_paths.c
+++ src/corgan_paths.c
@@ -4,9 +4,9 @@ int init_paths()
 {
     if (!getenv("XDG_DATA_HOME") || !strlen(getenv("XDG_DATA_HOME"))) {
         DATA_DIR = realloc(strdup(getenv("HOME")),
-                          sizeof(char) * (strlen(getenv("HOME"))
-                                          + strlen("/.local/share/corgan/")
-                                          + 1));
+                           sizeof(char) * (strlen(getenv("HOME"))
+                                           + strlen("/.local/share/corgan/")
+                                           + 1));
         if (!DATA_DIR) return -1;
 
         DATA_DIR = strcat(DATA_DIR, "/.local/");
@@ -20,23 +20,23 @@ int init_paths()
     }
     else {
         DATA_DIR = realloc(strdup(getenv("XDG_DATA_HOME")),
-                          sizeof(char) * (strlen(getenv("XDG_DATA_HOME"))
-                                          + strlen("/corgan/") + 1));
+                           sizeof(char) * (strlen(getenv("XDG_DATA_HOME"))
+                                           + strlen("/corgan/") + 1));
         if (!DATA_DIR) return -1;
 
         DATA_DIR = strcat(DATA_DIR, "/corgan/");
         mkdir(DATA_DIR, 0700);
     }
 
-    CONTACTS_PATH = realloc(strdup(DATA_DIR), sizeof(char)
-                                             * (strlen(DATA_DIR)
-                                                + strlen("contacts") + 1));
-    SCHEDULE_PATH = realloc(strdup(DATA_DIR), sizeof(char)
-                                             * (strlen(DATA_DIR)
-                                                + strlen("contacts") + 1));
-    EXPORT_PATH = realloc(strdup(DATA_DIR), sizeof(char)
-                                           * (strlen(DATA_DIR)
-                                              + strlen("contacts.vcf") + 1));
+    CONTACTS_PATH = realloc(strdup(DATA_DIR),
+                            sizeof(char) * (strlen(DATA_DIR)
+                                            + strlen("contacts") + 1));
+    SCHEDULE_PATH = realloc(strdup(DATA_DIR),
+                            sizeof(char) * (strlen(DATA_DIR)
+                                            + strlen("contacts") + 1));
+    EXPORT_PATH = realloc(strdup(DATA_DIR), 
+                          sizeof(char) * (strlen(DATA_DIR)
+                                          + strlen("contacts.vcf") + 1));
     if (!CONTACTS_PATH || !SCHEDULE_PATH || !EXPORT_PATH) return -1;
 
     CONTACTS_PATH = strcat(CONTACTS_PATH, "contacts");
