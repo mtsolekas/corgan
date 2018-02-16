4a0b14619972006e92c74768b51af5f6029d3774
Marios Tsolekas <marios.tsolekas@gmail.com>
2018-02-16 17:17:51 +0200

    Fixed i18n bug where it produced garbage in certain locales

--- src/corgan.c
+++ src/corgan.c
@@ -35,6 +35,7 @@ int main(int argc, char **argv)
 {
     setlocale(LC_ALL, "");
     bindtextdomain(PACKAGE, LOCALEDIR);
+    bind_textdomain_codeset(PACKAGE, "UTF-8");
     textdomain(PACKAGE);
 
     GObject *gobj;
