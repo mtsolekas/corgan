c483cb4e9e4b4fe9e88a5be437240fef6435e43b
Marios Tsolekas <marios.tsolekas@gmail.com>
2018-06-13 10:54:40 +0300

    Added schedule tests

---
+++ src/test_schedule.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2018 Marios Tsolekas <marios.tsolekas@gmail.com>
+ *
+ * This file is part of Corgan.
+ *
+ * Corgan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Corgan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Corgan.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Marios Tsolekas
+ */
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "paths.h"
+#include "schedule.h"
+
+int main()
+{
+    char *test_sched;
+
+    unsetenv("XDG_DATA_HOME");
+    setenv("HOME", "test_home", 1);
+
+    mkdir("test_home", 0777);
+    assert(!init_paths());
+
+    assert(!init_schedule());
+
+    test_sched = "TEST\n";
+    sched = test_sched;
+
+    assert(!write_schedule_file());
+
+    sched = "";
+    assert(!read_schedule_file());
+
+    assert(!strcmp(sched, test_sched));
+
+    return 0;
+}
